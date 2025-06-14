e0fa304ddfa6d12f4d37a21efff8285a863be120
Pratchayanon Thainngoen <feenfrink@gmail.com>
2025-06-14 14:59:21 +0700

    feat: setup font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@ const { heroui } = require("@heroui/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "var(--font-ibm)"],
+      },
+    },
   },
+
   darkMode: "class",
   plugins: [heroui()],
 };
