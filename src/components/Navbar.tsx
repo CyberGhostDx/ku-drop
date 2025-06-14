c08eb07d6470c81d0c1e139289e596f62a172342
Pratchayanon Thainngoen <feenfrink@gmail.com>
2025-06-14 15:52:15 +0700

    feat(navbar): add color transition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,16 +8,10 @@ const Navbar = () => {
   return (
     <nav className="fixed bg-white shadow-lg right-10 px-6 py-2 top-7 rounded-2xl items-center justify-between z-50">
       <div className="items-center justify-between">
-        <div className="hidden lg:flex gap-6 font-bold items-center justify-between text-lg">
-          <Link href="" className="hover:text-gray-400 duration-150">
-            หน้าเเรก
-          </Link>
-          <Link href="" className="hover:text-gray-400 duration-150">
-            ตารางเรียน
-          </Link>
-          <Link href="" className="hover:text-gray-400 duration-150">
-            การเดินทาง
-          </Link>
+        <div className="hidden lg:flex gap-6 font-bold items-center justify-between text-base *:hover:text-green-500 *:duration-150">
+          <Link href="">หน้าเเรก</Link>
+          <Link href="">ตารางเรียน</Link>
+          <Link href="">การเดินทาง</Link>
           <Button className="items-center justify-center bg-[#2AD349] text-white font-bold rounded-2xl text-base">
             Login
           </Button>
